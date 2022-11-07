a59e8f06cadc3616903df61d483f851610231d3f
ablzL <1822379342@qq.com>
2022-11-07 14:33:53 +0800

    Idea first commit

--- src/main/java/com/gxu/GitTest.java
+++ src/main/java/com/gxu/GitTest.java
@@ -3,5 +3,6 @@ package com.gxu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello IDEA Git!");
+        System.out.println("Hello IDEA Git!");
     }
 }
